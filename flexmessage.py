2b9ba71686473e039bd435f1c133decfee79b4ea
tcwang76 <59479227+tcwang76@users.noreply.github.com>
2020-05-24 23:08:05 +0800

    Update flexmessage.py
    add 活動名稱

--- flexmessage.py
+++ flexmessage.py
@@ -4,7 +4,27 @@
 
 
 #活動名稱
-
+#push到heroku上目前還沒成功QQ (by Tina)
+    if text == "活動名稱":
+        bubble = BubbleContainer(
+            direction = "ltr",
+            body = BoxComponent(
+                layout = "vertical",
+                contents = [
+                    TextComponent(text = "活動名稱", weight = "bold", size = "lg", align = "center"),
+                    BoxComponent(layout = "baseline", margin = "md", contents = [
+                        TextComponent(text = "請問您的活動名稱要叫什麼呢？", size = "md", flex = 0, color = "#666666")
+                        ]
+                                 )
+                    ]
+                )
+            )
+        
+        message = FlexSendMessage(alt_text = f"flex 活動名稱", contents = bubble)
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+            )
 
 #活動時間
 
