dda3620d57a1372c23f2b1b62f05194acc319a33
Lipet-dw <62240054+Lipet-dw@users.noreply.github.com>
2020-05-26 15:57:53 +0800

    Update flexmessage.py

--- flexmessage.py
+++ flexmessage.py
@@ -3,7 +3,7 @@
 #活動類型（選單）(by Tina)
     elif text == "活動類型":
         message = TextSendMessage(
-                text = "Quick reply",
+                text = "請選擇您的活動類型",
                 quick_reply = QuickReply(
                     items = [
                         QuickReplyButton(
@@ -42,16 +42,84 @@
                 )
             )
         
-        message = FlexSendMessage(alt_text = f"flex 活動名稱", contents = bubble)
+        message = FlexSendMessage(alt_text = "請填寫活動名稱", contents = bubble)
         line_bot_api.reply_message(
             event.reply_token,
             message
             )
 
 #活動時間
+if event.message.text == "活動時間":
+        bubble =BubbleContainer(
+                direction= "ltr",
+                body=BoxComponent(
+                  layout= "vertical",
+                  contents=[
+                  TextComponent(
+                      text="請選擇活動時間",
+                      size= "lg",
+                      align= "center",
+                      weight= "bold"
+                      )
+                  ]
+                ),
+                footer=BoxComponent(
+                  layout= "horizontal",
+                  contents= [
+                    ButtonComponent(
+                      DatetimePickerAction(
+                        label= "點我選時間",
+                        data="Activity_time",
+                        mode= "datetime",
+                        initial= "2020-05-26T15:25",
+                        max= "2021-05-26T15:25",
+                        min= "2019-05-26T15:25"
+                      )
+                    )
+                  ]
+                )
+            )
+        
+        message = FlexSendMessage(alt_text = "請挑選活動時間", contents = bubble)
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+            )
 
 
-#地點 (line.me/R/nv/location)
+#地點 (line.me/R/nv/location)(by Lipet)
+if text == "活動地點":
+        bubble =BubbleContainer(
+                direction= "ltr",
+                body=BoxComponent(
+                  layout= "vertical",
+                  contents=[
+                  TextComponent(
+                      text="請選擇活動地點",
+                      size= "lg",
+                      align= "center",
+                      weight= "bold"
+                      )
+                  ]
+                ),
+                footer=BoxComponent(
+                  layout= "horizontal",
+                  contents= [
+                    ButtonComponent(
+                      UriAction(
+                        label= "點我選地點",
+                        uri= "line.me/R/nv/location"
+                      )
+                    )
+                  ]
+                )
+            )
+        
+        message = FlexSendMessage(alt_text = "請挑選活動地點", contents = bubble)
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+            )
 
 
 #預期人數（最低最高）
